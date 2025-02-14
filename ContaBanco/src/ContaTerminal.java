ebc6df309baef789023ed3690817165de9b5c522
monicamendesraizen <monica.mendes@raizen.com>
2025-02-14 14:46:52 -0300

    implementacao classe ContaTerminal

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Scanner scanner = new Scanner(System.in);
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldoConta;
+
+        System.out.print("Por favor, digite o número da Conta !");
+        numero = scanner.nextInt();
+
+        System.out.print("Por favor, digite o número da Agência !");
+        agencia = scanner.nextLine();
+
+        System.out.print("Por favor, digite o nome do cliente !");
+        nomeCliente = scanner.nextLine();
+
+        System.out.print("Por favor, digite o saldo !");
+        saldoConta = scanner.nextDouble();
+
+        System.out.print("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldoConta + " já está disponível para saque");
+
     }
 }
